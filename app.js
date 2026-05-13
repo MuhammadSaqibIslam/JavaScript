019977e0f8a355725eddd902ba4a2e2c0e298203
Saqib Islam <106509321+MuhammadSaqibIslam@users.noreply.github.com>
2026-05-13 13:14:22 +0500

    first work

---
+++ app.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 15;
+let sum = a + b;
+console.log(sum);
+
+let aa = 23;
+let bb = 45;
+let minus = aa - bb;
+console.log(minus);
+
+let aaa = 896;
+let bbb = 445;
+let divid = aaa / bbb;
+console.log(divid)
+
+let aaaa = 24;
+let bbbb = 74;
+let multiply = aaaa * bbbb;
+console.log(multiply)
