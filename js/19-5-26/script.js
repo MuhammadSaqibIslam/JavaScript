5d4b616a0783ef0d0c8650420b249c434fd42b43
Saqib Islam <106509321+MuhammadSaqibIslam@users.noreply.github.com>
2026-05-19 17:29:01 +0500

    String Literls

--- js/19-5-26/script.js
+++ js/19-5-26/script.js
@@ -38,6 +38,187 @@ console.log("Saqib BMI:" , saqibBMI);
 console.log("Iqbal BMI:" , iqbalBMI);
 
 
+//String Literals
+
+/*1.
+
+Write a program that prints this using string literals:
+
+My name is "Your Name"
+I am learning JavaScript
+I will practice daily
+*/
+
+let myName = "Saqib Islam"
+console.log(`My name is ${myName} I am learning JavaScript I will practice daily`);
 
 
- 
+/* 2.
+
+Create a variable called studentName
+
+Use string literals to print:
+
+Hello, My name is studentName
+*/
+
+let studentName = "Saqib Islam "
+console.log(`Hello, My name  is ${studentName}`);
+
+/* 3.
+
+Create two variables:
+
+one for your city
+one for your favorite language
+
+Print them in one sentence using string literals.
+
+Example format:
+I live in Faisalabad and I love JavaScript
+*/
+
+let city = "Faisalaabd";
+let language = "JavaScript";
+console.log(`I live in ${city} and I love ${language}`);
+
+
+/*
+Create a variable called goal
+
+Print this sentence using string literals:
+My goal is to become a Frontend Developer
+*/
+
+let goal = "Frontend Developer";
+console.log(`My goal is to become a ${goal}`);
+
+/*
+5.
+
+Create two number variables and print this using string literals:
+The sum of 10 and 20 is 30
+*/
+let num1 = 10;
+let num2 = 20;
+console.log(`The sum of ${num1} and ${num2} is 30`);
+
+/*String Literals Practice – Part 2
+Coding Challenge
+
+1.
+
+Create a variable called favoriteGame
+
+Print this sentence using string literals:
+
+My favorite game is PUBG
+*/
+
+let favoriteGame = "Cricut";
+console.log(`My favorite game is ${favoriteGame}`);
+
+/* 2.
+
+Create two variables:
+
+bestFriend
+favoriteDrink
+
+Print them in one sentence.
+
+Example format:
+Ali likes Coffee
+*/
+
+let bestFriend = "Iqbal";
+let favoriteDrink = "Tea";
+console.log(`${bestFriend} likes ${favoriteDrink}`);
+
+
+/*
+3.
+
+Create a variable called dreamCompany
+
+Print this sentence:
+I want to work at Google
+*/
+
+let dreamCompany = "Google";
+console.log(`I want to work at ${dreamCompany}`);
+
+/*
+4.
+
+Create three variables:
+
+morningMeal
+eveningSnack
+nightDrink
+
+Print all of them in one sentence using string literals.
+
+Example format:
+I eat Pizza, Chips and Juice
+*/
+
+let morningMeal = "Pizza";
+let eveningSnack = "Chips";
+let nightDrink = "Juice";
+console.log(`I eat ${morningMeal}, ${eveningSnack} and ${nightDrink}`);
+
+/*
+5.
+
+Create two number variables.
+
+Print this type of sentence:
+The multiply of 5 and 4 is 20
+*/
+
+let num3 = 5;
+let num4 = 4;
+console.log(`The multiply of ${num3} and ${num4} is 20`);
+
+/*
+6.
+
+Create a variable called currentSkill
+
+Print this sentence:
+Right now I am learning JavaScript
+*/
+
+let currentSkill = "JavaScript";
+console.log(`Right now I am learning ${currentSkill}`);
+
+/*
+7.
+
+Create two variables:
+
+favoriteCar
+carColor
+
+Print this sentence:
+My favorite car is Civic and its color is Black
+*/
+
+let favoriteCar = "Civic";
+let carColor = "Black";
+
+
+/*
+8.
+
+Create a variable called weekendPlan
+
+Print this sentence:
+
+My weekend plan is coding practice
+*/
+
+let weekendPlan = "coding practice";
+console.log(`My weekend plan is ${weekendPlan}`);
+
