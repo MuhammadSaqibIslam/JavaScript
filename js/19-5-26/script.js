e425ca58d8b0853e7caf5379b30fe4e110c55972
Saqib Islam <106509321+MuhammadSaqibIslam@users.noreply.github.com>
2026-05-19 16:14:01 +0500

    Coding challange 1

--- js/19-5-26/script.js
+++ js/19-5-26/script.js
@@ -1,3 +1,43 @@
 let a = 23;
 let b = 12;
 console.log(a+b);
+
+
+console.log("Practice makes me stronger!");
+
+console.log("My name is Saqib Islam I am learning JavaScript daily I will become a good developer Consistency is the key to success");
+
+ let food = "Mix Fruites";
+ let age = 38;
+ console.log("My favorite food is " + food + "and my age is " + age);
+
+
+let c = 45;
+let d = 32;
+let sum = c + d;
+let subtraction = c - d;
+let multiplication = c * d;
+let division = c / d;
+console.log(sum);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+
+// Data or Saqib
+let saqibMass = 80;
+let saqibHeight = 5.4;
+
+// Data of Iqbal
+let iqbalMass = 65;
+let iqbalHeight = 5.2;
+
+let saqibBMI = saqibMass / (saqibHeight * saqibHeight);
+let iqbalBMI = iqbalMass / (iqbalHeight * iqbalHeight);
+
+console.log("Saqib BMI:" , saqibBMI);
+console.log("Iqbal BMI:" , iqbalBMI);
+
+
+
+
+ 
