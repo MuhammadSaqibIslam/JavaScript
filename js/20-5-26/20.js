37cc398d2197340563a7abfb48028d952bf42151
Saqib Islam <106509321+MuhammadSaqibIslam@users.noreply.github.com>
2026-05-20 18:19:09 +0500

    add some conditions

---
+++ js/20-5-26/20.js
@@ -0,0 +1,438 @@
+//Condition
+
+/*
+1.
+
+Create a variable called temperature
+
+agar temperature 30 se zyada ho
+→ "Hot"
+warna
+→ "Normal"
+*/
+
+let temperature = 38;
+if (temperature > 30){
+    console.log("Hot");
+}
+else{
+    console.log("Normal"); 
+}
+
+/*
+2.
+
+Create a variable called marks
+
+agar marks 50 ya zyada hon
+→ "Pass"
+warna
+→ "Fail"
+*/
+
+let marks = 60;
+if (marks > 50){
+    console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
+
+/*
+3.
+
+Create a variable called walletMoney
+
+agar money 1000 se zyada ho
+→ "Shopping Possible"
+warna
+→ "Low Budget"
+*/
+
+let walletMoney = 1500;
+if(walletMoney > 1000){
+    console.log("Shopping Possible"); 
+}
+else{
+    console.log("Low Budget");
+}
+
+/*
+4.
+
+Create a variable called internetConnected
+
+agar internet connected ho
+→ "Online"
+warna
+→ "Offline"
+*/
+
+let internetConnected = true;
+if (internetConnected){
+    console.log("Online");
+}
+else{
+    console.log("Offline");
+}
+
+/*
+5.
+
+Create a variable called screenBrightness
+
+agar brightness 70 se zyada ho
+→ "Bright Screen"
+warna
+→ "Dim Screen"
+*/
+
+let screenBrightness = 80;
+if(screenBrightness >70){
+    console.log("Brigth Screen");
+}
+else{
+    console.log("Dim Screen");
+}
+
+/*
+6.
+
+Create a variable called userAge
+
+agar age 18 ya zyada ho
+→ "Adult"
+warna
+→ "Minor"
+*/
+
+let userAge= 20;
+if(userAge >18){
+    console.log("Adult");
+}
+else{
+    console.log("Minor");
+}
+
+/*
+7.
+
+Create a variable called gameInstalled
+
+agar game installed ho
+→ "Ready to Play"
+warna
+→ "Install Game"
+*/
+
+let gameInstalled = true;
+if(gameInstalled){
+    console.log("Ready to Play");
+}
+else{
+    console.log("Install Game");
+}
+
+/*
+8.
+
+Create a variable called phoneStorage
+
+agar storage 128 ya zyada ho
+→ "Enough Storage"
+warna
+→ "Storage Full"
+*/
+
+let phoneStorage = 150;
+if (phoneStorage >128){
+    console.log("Enough Storage");
+}
+else{
+    console.log("Storeage Full");
+}
+
+/*
+1.
+
+Create a variable called fanOn
+
+agar fan on ho
+→ "Cooling Started"
+warna
+→ "Fan is Off"
+*/
+
+let fanOn = true;
+if(fanOn){
+    console.log("Cooling Started");
+}
+else{
+    console.log("Fan is off");
+}
+
+/*
+2.
+
+Create a variable called waterBottle
+
+agar bottle me water 1 liter se zyada ho
+→ "Enough Water"
+warna
+→ "Refill Bottle"
+*/
+
+let waterBottle = 2;
+if(waterBottle > 1){
+    console.log("Enough Water");
+}
+else{
+    console.log("Refill Bottle");
+}
+
+/*
+3.
+
+Create a variable called homeLight
+
+agar light on ho
+→ "Room Bright"
+warna
+→ "Room Dark"
+*/
+
+let homeLight = false;
+if(homeLight){
+    console.log("Room Bright");
+}
+else{
+    console.log("Room Dark");
+}
+
+/*
+4.
+
+Create a variable called ticketPrice
+
+agar ticket price 500 ya us se kam ho
+→ "Affordable"
+warna
+→ "Expensive"
+*/
+
+let ticketPrice = 550;
+if (ticketPrice > 500){
+    console.log("Affordable");
+}
+else{
+    console.log("Expensive");
+}
+
+/*
+5.
+
+Create a variable called mobileVolume
+
+agar volume 80 se zyada ho
+→ "High Volume"
+warna
+→ "Normal Volume"
+*/
+
+let mobileVolume = 90;
+if(mobileVolume > 80){
+    console.log("High Volume");
+}
+else{
+    console.log("Noraml Volume");
+}
+
+/*
+6.
+
+Create a variable called bikeFuel
+
+agar fuel 3 liter se zyada ho
+→ "Ready for Ride"
+warna
+→ "Need Fuel"
+*/
+
+let bikeFuel = 4;
+if(bikeFuel){
+    console.log("Ready for Ride ");
+}
+else{
+    console.log("Need Fuel");
+}
+
+/*
+7.
+
+Create a variable called laptopCharging
+
+agar laptop charging par ho
+→ "Charging"
+warna
+→ "Not Charging"
+*/
+
+let laptopCharging = true;
+if(laptopCharging){
+    console.log("Charging");
+}
+else{
+    console.log("Not Charging");
+}
+
+/*
+8.
+
+Create a variable called downloadSpeed
+
+agar speed 50 ya zyada ho
+→ "Fast Download"
+warna
+→ "Slow Download"
+*/
+
+let downloadSpeed = 60;
+if(downloadSpeed > 50){
+    console.log("Fast Download");
+}
+else{
+    console.log("Slow Download");
+}
+
+/*
+9.
+
+Create a variable called weatherTemp
+
+agar temperature 15 se kam ho
+→ "Cold Weather"
+warna
+→ "Normal Weather"
+*/
+
+let weatherTemp =21;
+if(weatherTemp > 15){
+    console.log("Cold Weather");
+}
+else{
+    console.log("Normal Weather");
+}
+
+/*
+10.
+
+Create a variable called alarmActive
+
+agar alarm active ho
+→ "Security ON"
+warna
+→ "Security OFF"
+*/
+
+let alarmActive = true;
+if(alarmActive){
+    console.log("Security ON");
+}
+else{
+    console.log("Security OFF");
+}
+
+/*
+1.
+
+Create a variable called roomTemp
+
+agar temperature 25 se zyada ho
+→ "Hot Room"
+warna
+→ "Cool Room"
+*/
+
+let roomTemp = 32;
+if(roomTemp > 25){
+    console.log("Hot Room");
+}
+else{
+    console.log("Cool Room");
+}
+
+/*
+2.
+
+Create a variable called studentMarks
+
+agar marks 40 ya zyada ho
+→ "Pass"
+warna
+→ "Fail"
+*/
+
+let studentMarks = 50;
+if(studentMarks >= 40){
+console.log("Pass");
+}
+else{
+    console.log("Fail");
+}
+
+/*
+3.
+
+Create a variable called enteredCode
+
+agar code "1234" ke equal ho
+→ "Access Granted"
+warna
+→ "Wrong Code"
+*/
+
+let enteredCode = 1234;
+if(enteredCode === 1234){
+    console.log("Access Granted");
+}
+else{
+    console.log("Wrong Code");
+}
+
+/*
+4.
+
+Create a variable called dailySteps
+
+agar steps 10000 se zyada hon
+→ "Great Job"
+warna
+→ "Keep Walking"
+*/
+
+let dailySteps = 12000;
+if(dailySteps > 10000){
+    console.log("Great Job");
+}
+else{
+    console.log("Keep Walking");
+}
+
+/*
+5.
+
+Create a variable called deviceBattery
+
+agar battery 20 ya us se kam ho
+→ "Low Battery"
+warna
+→ "Battery OK"
+*/
+
+let deviceBattery = 12;
+if(deviceBattery < 20){
+    console.log("Low Battery");
+}
+else{
+    console.log("Bettery OK");
+}
