8f337b65c578f89dcf915058d773457a64ed8d63
Saqib Islam <106509321+MuhammadSaqibIslam@users.noreply.github.com>
2026-06-09 18:36:18 +0500

    added loops practice

--- js/25-5-26/Loops.js
+++ js/25-5-26/Loops.js
@@ -7,3 +7,7 @@
 for ( let i = 2; i < 10; i+2){
     console.log(i);
 }
+
+for (let i = 0; i < 5; i++){
+    console.log(i); 
+}
---
+++ js/june/8-6-26/Loops 1.js
@@ -0,0 +1,71 @@
+// for( let i = 0; i < 5; i++){
+//     console.log(i);
+// }
+
+// for ( let i = 1; i < 11; i++){
+//     console.log(i);
+// }
+
+// let name = "saqib";
+// console.log(name);
+
+
+// for(let i = 1; i <= 10; i++){
+// console.log('2 x', i,' = ', i * 2);
+// }
+
+/*
+Question 1
+
+1 se 10 tak numbers print karo:
+
+1
+2
+3
+...
+10
+*/
+
+for( let i = 1; i <= 10; i++){
+console.log(i);
+}
+
+/*
+Question 2
+
+10 se 1 tak ulta print karo:
+
+10
+9
+8
+...
+1
+*/
+for( let i =10; i >= 1; i--){
+console.log(i);
+}
+
+/*
+5 ka table print karo:
+
+5 x 1 = 5
+5 x 2 = 10
+...
+5 x 10 = 50
+*/
+
+for( let i = 1; i <= 10; i++){
+console.log('5 x', i  ,'=', i*5);
+}
+
+// Sirf even numbers print karo:
+
+// 2
+// 4
+// 6
+// 8
+// 10
+
+for( let i = 2; i <=10; i = i+2){
+console.log(i);
+}
