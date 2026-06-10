d8fe3bf6ca800cfd1cebc3eae0331c1bd0452efd
Your Name <you@example.com>
2026-06-10 23:05:06 +0500

    Array practice

---
+++ js/june/10-6-26/Array.js
@@ -0,0 +1,33 @@
+let userName=["Saqib","Islam", "Muna","Jiya","Mian","Jojo"]
+let userAge = [38, 65, 11, 7, 5, 1.5]
+console.log(userName);
+console.log(userAge);
+for(let i = 0; i < 6; i++)
+console.log(`${userName[i]} is ${userAge[i]} years old`);
+
+/*
+let studentName = ["Ahmed", "Usman", "Bilal", "Hamza"];
+let studentMarks = [80, 75, 90, 85];
+*/
+
+let studentName = ["Ahmad", "Usman", "Bilal", "Hamza"];
+let studentMarks = [80, 75, 90, 85];
+console.log(studentName);
+console.log(studentMarks);
+for(let i = 0; i < 4; i++)
+console.log(`${studentName[i]} got ${studentMarks[i]} marks`);
+
+/*
+let cityName = ["Lahore", "Karachi", "Islamabad"];
+let population = [13000000, 18000000, 1200000];
+*/
+let cityName = ["Lahore", "Karachi", "Islamabad"];
+let population = [130000, 180000, 120000];
+for (let i = 0; i < 3; i++);
+console.log(`${cityName[i]} has population ${population[i]}`);
+
+
+
+
+
+
