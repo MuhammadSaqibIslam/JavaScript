d0f0ff1a44ddc980b85502acb2902947fdae09be
Saqib Islam <106509321+MuhammadSaqibIslam@users.noreply.github.com>
2026-07-15 18:04:51 +0500

    dom completed

---
+++ Dom/app.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+function darkMode(){
+let container = document.getElementById("container");
+let lightImg = document.getElementById("lightImg");
+let darkImg = document.getElementById("darkImg");
+
+container.style.backgroundColor ="black";
+container.style.color="white";
+
+lightImg.style.display = "none";
+darkImg.style.display="inline";
+}
+function lightMode(){
+let container =document.getElementById("container");
+let lightImg =document.getElementById("lightImg");
+let darkImg=document.getElementById("darkImg");
+
+container.style.backgroundColor ="white";
+container.style.color="black";
+
+lightImg.style.display = "inline";
+darkImg.style.display ="none";
+}
